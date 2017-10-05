d2c970eaee3c4b98ee754aa1789eeb0e3b7c3547
Christopher Ingram <cingram82@gmail.com>
2017-10-05 08:56:01 -0400

    upgraded to 5x5 grid changed start of game logic to work with any size

--- LightsOut.js
+++ LightsOut.js
@@ -1,57 +1,78 @@
-let startGameButton = document.querySelector("#gameStartButton");
-startGameButton.addEventListener("click", startNewGame);
-let lightsOutPlayGrid = document.querySelector("#lightsOutGrid");
+let startGameButton = document.querySelector('#gameStartButton');
+startGameButton.addEventListener('click', startNewGame);
+let lightsOutPlayGrid = document.querySelector('#lightsOutGrid');
 let arrayOfLights = [];
 let winMessage = document.querySelector('h1');
-
+let gameSize = 5;
+let gridSize = gameSize * gameSize;
 function startNewGame() {
   grid = '';
-  winMessage.classList = '';
-
-  for (var i = 1; i < 10; i++) {
-    // if (Math.round(Math.random()) === 1) {
-    //   grid += `<div class="lightBox" id="lightBox${i}">
-    // </div>`
-    // } else {
-      grid += `<div class="lightBox active" id="lightBox${i}">
-    </div>`
-    // }
+  winMessage.classList;
+  for (var i = 1; i < (gridSize + 1); i++) {
+    grid += `<div class='lightBox' data-id='${i}'>
+    </div>`;
   }
 
   lightsOutPlayGrid.innerHTML = grid;
   arrayOfLights = document.getElementsByClassName('lightBox');
-  for (var i = 0; i < arrayOfLights.length; i++) {
-    buttonsChange(arrayOfLights[i])
-  }
+  newGameSetUp();
 }
 
-
-
-lightsOutPlayGrid.addEventListener("click", buttonsChange);
-
-function buttonsChange(buttonsChange) {
+lightsOutPlayGrid.addEventListener('click', function (buttonsChange) {
   currentClass = buttonsChange.target;
-  currentClass.classList.toggle("active")
-  self = parseInt(currentClass.id.slice(-1));
+  currentClass.classList.toggle('active');
+  self = parseInt(currentClass.dataset.id);
 
-  if (self - 3 > 0) {
-    arrayOfLights[self - 4].classList.toggle("active");
+  if (self - gameSize > 0) {
+    changeLightUp();
   }
-  if (self + 3 < 10) {
-    arrayOfLights[self + 2].classList.toggle("active");
+
+  if (self + gameSize < gridSize + 1) {
+    changeLightDown();
   }
-  if (self % 3 === 1) {
-    arrayOfLights[self].classList.toggle("active");
-  } else if (self % 3 === 0) {
-    arrayOfLights[self - 2].classList.toggle("active");
+
+  if (self % gameSize === 1) {
+    changeLightLeft();
+  } else if (self % gameSize === 0) {
+    changeLightRight();
   } else {
-    arrayOfLights[self].classList.toggle("active");
-    arrayOfLights[self - 2].classList.toggle("active");
+    changeLightLeft();
+    changeLightRight();
   }
+
   for (var i = 0; i < arrayOfLights.length; i++) {
-    if (arrayOfLights[i].classList[1] !== "active") {
+    if (arrayOfLights[i].classList[1] !== 'active') {
       return;
     }
   }
-  winMessage.classList.toggle("active");
-};
+
+  youWin();
+});
+
+function newGameSetUp() {
+  for (i = 1; i < gridSize + 1; i++) {
+    if (Math.random() > 0.5) {
+      arrayOfLights[i].classList.toggle('active');
+    }
+  }
+}
+
+function changeLightUp() {
+  arrayOfLights[self - gameSize - 1].classList.toggle('active');
+}
+
+function changeLightDown() {
+  arrayOfLights[self + gameSize - 1].classList.toggle('active');
+}
+
+function changeLightLeft() {
+  arrayOfLights[self].classList.toggle('active');
+}
+
+function changeLightRight() {
+  arrayOfLights[self - 2].classList.toggle('active');
+}
+
+function youWin() {
+  winMessage.classList.toggle('active');
+}
